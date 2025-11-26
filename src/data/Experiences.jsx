1b2dcae5f1249d80f26a525b93faa36a650e21ef
Johnny <thanhtrancoder@gmail.com>
2025-11-27 00:13:38 +0700

    update experience

--- src/data/Experiences.jsx
+++ src/data/Experiences.jsx
@@ -1,24 +1,22 @@
 const Experiences = [
   {
-    title: "Intern - GSOFT",
+    title: ".NET Programming Intern",
     description: (
       <div>
-        <p className="mb-2">Global Software Joint Stock Company</p>
+        <p className="mb-2">GSOFT - Global Software Joint Stock Company</p>
         <ul className="list-disc ml-4 space-y-2">
-          <li>Participate in developing RESTful APIs using Spring Boot.</li>
+          <li>Developed RESTful APIs using ASP.NET.</li>
           <li>Fix assigned bugs.</li>
         </ul>
       </div>
     ),
-    year: "2023",
+    year: "2022",
   },
   {
-    title: "Remote - WISDOM",
+    title: "Backend Developer part-time",
     description: (
       <div>
-        <p className="mb-2">
-          Wisdom Engineering and Business Solutions Company
-        </p>
+        <p className="mb-2">Wisdom Engineering & Business Solutions</p>
         <ul className="list-disc space-y-2 ml-4 md:hidden">
           <li className="">
             Participate in functional design and database development for the
@@ -38,25 +36,22 @@ const Experiences = [
         </ul>
       </div>
     ),
-    year: "2024",
+    year: "2023",
   },
   {
-    title: "Graduate - HCMUTE",
+    title: "Graduated - HCMUTE",
     description: (
       <div>
         <p className="mb-2">
           Ho Chi Minh City University of Technology and Education
         </p>
         <ul className="list-disc ml-4 space-y-2">
-          <li>
-            Graduated from Ho Chi Minh City University of Technology and
-            Education.
-          </li>
+          <li>Graduated with a degree in information technology.</li>
           <li>
             Relevant Coursework: Data Structures & Algorithms, Web Programming,
             Database Systems, Object-Oriented Programming, Software Engineering.
           </li>
-          <li>GPA: 7.48/10.00</li>
+          <li>CGPA: 7.48/10</li>
         </ul>
       </div>
     ),
